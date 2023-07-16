977b7d60652169301ea287e190127cbf29ef7faa
cerebralspill <mikengoc@gmail.com>
2023-07-16 09:06:02 -0400

    Added some style and built list

--- src/ToChewList.tsx
+++ src/ToChewList.tsx
@@ -1,13 +1,40 @@
 import React, { useState } from "react";
 
-export const ToChewList = () => {
-    return <div className="main-container">
-        <h1>ToChewList</h1>;
-        <ul>
-            <li>item 1</li>
-            <li>item 2</li>
-        </ul>
+interface item {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+export const ToChewList: React.FC = () => {
+    const [tochews,setTochews] = useState<item[]>([
+        { id: 1, text: "Make pancakes", completed: false },
+        { id: 2, text: "Scramble eggs", completed: false }
+    ]);
+
+    const handleToggle = (id:number) => {
+        setTochews(
+            tochews.map((tochew) => {
+                if(tochew.id === id) {
+                    return { ...tochew, completed: !tochew.completed }
+                }
+                return tochew;
+            })
+        )
+    }
+
+    return (
+        <div className="main-container">
+            <h1>ToChewList</h1>;
+            <ul>
+                {tochews.map((tochew) => (
+                    <li key={tochew.id} onClick={() => handleToggle(tochew.id)}>
+                    {tochew.text}
+                    </li>
+                ))}
+            </ul>
         <input type="text" placeholder="Add tochew item" />
         <button>Add</button>
-        </div>;
+        </div>
+    );
 };
